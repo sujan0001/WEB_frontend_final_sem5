471a16dbc92475f4b050d4c084b9cb31e058a897
sujan bhujel <sujanbhujel1333@gmail.com>
2025-06-21 21:12:58 +0545

    app router

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,7 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
-import Homepage from "../pages/Homepage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Homepage from "../pages/Homepage";
+import Dashboard from "../components/admin/Dashboard";
+import UserManagement from "../pages/admin/UserManagement";
+import CategoryManagement from "../pages/admin/CategoryManagement";
+import ProductManagement from "../pages/admin/ProductManagement";
+import OrderManagement from "../pages/admin/OrderManagement";
 
 export default function AppRouter() {
   return (
@@ -10,8 +15,16 @@ export default function AppRouter() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Homepage />} />
+
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="users" element={<UserManagement />} />
+          <Route path="categories" element={<CategoryManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="orders" element={<OrderManagement />} />
+        </Route>
+
         <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
